7cb0407afe293140b3f79cf2861d1b63aaaf86de
aozora <marcellop@protonmail.com>
2021-01-17 17:36:49 +0100

    chore: prettified everything

--- 01-testing-setup/src/components/Search.js
+++ 01-testing-setup/src/components/Search.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import SearchForm from "./SearchForm";
-import SearchResults from "./SearchResults";
-import data from "../data/mock-data.json";
+import React, { useState } from 'react';
+import SearchForm from './SearchForm';
+import SearchResults from './SearchResults';
+import data from '../data/mock-data.json';
 
 const Search = () => {
   const [items, setItems] = useState([]);
